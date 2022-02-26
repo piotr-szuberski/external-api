1ef25568cfb83d27140ee64cdb0831dcf6bf9381
pjotrekk <pjotrekk@protonmail.com>
2022-02-26 16:32:03 +0100

    feat: add spotless formatter

--- src/main/java/com/pet/project/external/api/ExternalApiApplication.java
+++ src/main/java/com/pet/project/external/api/ExternalApiApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ExternalApiApplication {
 
-	public static void main(String... args) {
-		SpringApplication.run(ExternalApiApplication.class, args);
-	}
-
+  public static void main(String... args) {
+    SpringApplication.run(ExternalApiApplication.class, args);
+  }
 }
